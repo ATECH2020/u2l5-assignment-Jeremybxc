86fe30629f886835c8e1666cb991f21d80878485
Jeremy Brandon Xavier Co <jeremyb.228217@nv.ccsd.net>
2020-10-01 17:00:06 +0000

    Started code

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,16 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        double tRat;
+        int wind;
+        int lcost;
+        double tot;
+        double gtot;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the sales tax rate: ");
+        s
+
     }
 }
