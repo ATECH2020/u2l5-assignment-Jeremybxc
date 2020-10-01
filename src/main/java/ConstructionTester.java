f6b931e1905d65894e620220642cccb0f6b973ef
Jeremy Brandon Xavier Co <jeremyb.228217@nv.ccsd.net>
2020-10-01 23:07:19 +0000

    Finished Construction tester

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -6,14 +6,23 @@ public class ConstructionTester
     {
         double tRat;
         int wind;
-        int lcost;
+        int bord;
         double tot;
         double gtot;
 
-        Scanner in = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter the sales tax rate: ");
-        s
+        System.out.println("Enter the sales tax rate: ");
+        tRat = scan.nextDouble();
+        tRat = tRat + 1;
+        System.out.println("How many boards do you need? ");
+        bord = scan.nextInt();
+        System.out.println("How many windows do you need? ");
+        wind = scan.nextInt();
+        tot = (wind * 11) + (bord * 8);
+        System.out.println("Total: " + tot);
+        gtot = tot * tRat;
+        System.out.print("Grand Total: " + gtot);
 
     }
 }
