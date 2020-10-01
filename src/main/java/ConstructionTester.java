1fc3c20f91247a9c650f5c66b0efd43c0d657848
Jeremy Brandon Xavier Co <jeremyb.228217@nv.ccsd.net>
2020-10-01 23:55:21 +0000

    Tried to fix code

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class ConstructionTester
 {
     public static void main(String[] args)
@@ -7,21 +8,26 @@ public class ConstructionTester
         double tRat;
         int wind;
         int bord;
+        double bordc;
+        double winc;
         double tot;
         double gtot;
 
         Scanner scan = new Scanner(System.in);
 
+        
         System.out.println("Enter the sales tax rate: ");
         tRat = scan.nextDouble();
-        tRat = tRat + 1;
+        Construction(8, 11, tRat);
         System.out.println("How many boards do you need? ");
         bord = scan.nextInt();
+        bordc = Construction.lumberCost(bord);
         System.out.println("How many windows do you need? ");
         wind = scan.nextInt();
-        tot = (wind * 11) + (bord * 8);
+        winc = Construction.windowCost(wind);
+        tot = winc + bordc;
         System.out.println("Total: " + tot);
-        gtot = tot * tRat;
+        gtot = Construction.grandTotal(tot);
         System.out.print("Grand Total: " + gtot);
 
     }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,23 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        double slat;
+        double slon;
+        double elat;
+        double elon;
+        double dis;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the latitude of the starting location: ");
+        slat = scan.nextDouble();
+        System.out.print("Enter the longitude of the starting location: ");
+        slon = scan.nextDouble();
+        System.out.print("Enter the latitude of the ending location: ");
+        elat = scan.nextDouble();
+        System.out.print("Enter the longitude of the ending location: ");
+        elon = scan.nextDouble();
+        
+        System.out.print("The distance is " + dis);
     }
 }
